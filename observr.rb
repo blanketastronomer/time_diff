9bb61640f86e41e701b86935925386caf6682aad
Nicholas S <blanketastronomer@users.noreply.github.com>
2016-01-13 12:32:33 -0800

    Add Observr as a development dependency

---
+++ observr.rb
@@ -0,0 +1,21 @@
+# time_diff - A pure ruby library that converts the difference between two dates and times into various formats.
+# Copyright (C) 2016  blanketastronomer <blanketastronomer@users.noreply.github.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+test_regex = /test\/.*_test\.rb/
+lib_regex = /lib\/(.*)\.rb/
+
+watch(test_regex) { |md| system("ruby #{md[0]}") }
+watch(lib_regex) { |md| system("ruby test/#{md[1]}_test.rb") }
